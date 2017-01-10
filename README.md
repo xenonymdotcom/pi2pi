6d9365d0810429030acbecf10186b9f672c511e4
xenonymdotcom <github@xenonym.com>
2017-01-10 21:12:34 +0000

    added simple commms

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ simple Raspberry Pi to Raspberry PI comms software
 
 This project (once written) will allow 2 Raspberry pis to communicate 
 back to back using `/dev/ttyAMA0` serial coms
+
+make sure you are in the tty group 
+use `sudo usermod -a -G tty <yourname>`
+
+and may sure that `/dev/ttyAMA0` is readable by the tty group
+use `sudu chmod g+r /dev/ttyAMA0` if it is only writeable
+by the tty group as mine was.
--- main.c
+++ main.c
@@ -1,6 +1,8 @@
+#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int open_serial_port()
+FILE * open_serial_port( void )
 {
 	int fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NONBLOCK);
 	if ( fd < 0 )
@@ -8,12 +10,45 @@ int open_serial_port()
 		printf("unable to open /dev/ttyAMA0 (do you have the correct rights)");
 		exit(1);
 	}
+	return fdopen(fd, "rw");
 }
 
+void send_hello( FILE * f )
+{
+	fprintf( f, "hello from client" );
+}
+
+void listen_and_respond( FILE * f, int mode )
+{
+	static counter;
+	char buffer[256];
+	int n = fscanf( f, "%255s", buffer );
+	printf( "recv(%d):%s;;\n", n, buffer);
+	if ( mode )
+	{
+		sleep(1);
+		fprintf( f, "hello %d again", counter++ );
+	}
+	else
+	{
+		counter += n;
+		fprintf( f, "recv(%d)", counter );
+	}
+}
 
 int main( int argc, char * argv[] )
 {
-	int sp = open_serial_port();
-	printf("serial port handle = %d;;", sp);
+	FILE * f = open_serial_port();
+	printf("serial port handle = %d;;\n", (int)f);
+
+	if ( !f ) exit( 1 );
+
+	if (argc > 1)
+	{
+		send_hello( f );
+	}
+	listen_and_respond( f, (argc > 1) );
+
 	return 0;
 }
+
